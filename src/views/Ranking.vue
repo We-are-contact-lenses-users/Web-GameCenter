cb0edb836afd0a5b97acc7bf2b2694040b7e6bce
non0829 <u474334e@ecs.osaka-u.ac.jp>
2021-06-17 18:17:06 +0900

    ランキング、投稿画面の見た目

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -2,8 +2,19 @@
   <div id="app">
     <StarBackGround></StarBackGround>
     <div class="top-menu">
-      <div>Ranking</div>
-      <router-link to="/selectgame">SelectGame</router-link>
+      <h1 class="title">Ranking</h1>
+      <ul class="rankings">
+        <li v-for="(ranking, index) in rankings" :key="index" class="rankingBox">
+          <h2>{{ranking.title}}</h2>
+          <table>
+            <tr v-for="(rank, index2) in ranking" :key="index * 10 + index2">
+              <td>{{rank.rank}}</td>
+              <td>{{rank.person}}</td>
+              <td>{{rank.result}}</td>
+            </tr>
+          </table>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -11,6 +22,181 @@
 import StarBackGround from "@/components/StarBackGround.vue";
 
 export default {
+  // ここのデータをfirebaseから引っ張り出す
+  data() {
+    return {
+      rankings: {
+        allranking: {
+          title: '総合ランキング',
+          first: {
+            rank: "1位",
+            person: "入賞者",
+            result: 0
+          },
+          second: {
+            rank: "2位",
+            person: "入賞者",
+            result: 0
+          },
+          third: {
+            rank: "3位",
+            person: "入賞者",
+            result: 0
+          },
+          fourth: {
+            rank: "4位",
+            person: "入賞者",
+            result: 0
+          },
+          fifth: {
+            rank: "5位",
+            person: "入賞者",
+            result: 0
+          },
+        },
+        puzzlegame: {
+          title: "Puzzle Game",
+          first: {
+            rank: "1位",
+            person: "入賞者",
+            result: 0
+          },
+          second: {
+            rank: "2位",
+            person: "入賞者",
+            result: 0
+          },
+          third: {
+            rank: "3位",
+            person: "入賞者",
+            result: 0
+          },
+          fourth: {
+            rank: "4位",
+            person: "入賞者",
+            result: 0
+          },
+          fifth: {
+            rank: "5位",
+            person: "入賞者",
+            result: 0
+          },
+        },
+        typinggame: {
+          title: "Typing Game",
+          first: {
+            rank: "1位",
+            person: "入賞者",
+            result: 0
+          },
+          second: {
+            rank: "2位",
+            person: "入賞者",
+            result: 0
+          },
+          third: {
+            rank: "3位",
+            person: "入賞者",
+            result: 0
+          },
+          fourth: {
+            rank: "4位",
+            person: "入賞者",
+            result: 0
+          },
+          fifth: {
+            rank: "5位",
+            person: "入賞者",
+            result: 0
+          },
+        },
+        pinballgame: {
+          title: 'Pinball Game',
+          first: {
+            rank: "1位",
+            person: "入賞者",
+            result: 0
+          },
+          second: {
+            rank: "2位",
+            person: "入賞者",
+            result: 0
+          },
+          third: {
+            rank: "3位",
+            person: "入賞者",
+            result: 0
+          },
+          fourth: {
+            rank: "4位",
+            person: "入賞者",
+            result: 0
+          },
+          fifth: {
+            rank: "5位",
+            person: "入賞者",
+            result: 0
+          },
+        },
+        quizgame: {
+          title: "Quiz Game",
+          first: {
+            rank: "1位",
+            person: "入賞者",
+            result: 0
+          },
+          second: {
+            rank: "2位",
+            person: "入賞者",
+            result: 0
+          },
+          third: {
+            rank: "3位",
+            person: "入賞者",
+            result: 0
+          },
+          fourth: {
+            rank: "4位",
+            person: "入賞者",
+            result: 0
+          },
+          fifth: {
+            rank: "5位",
+            person: "入賞者",
+            result: 0
+          },
+        },
+        snipergame: {
+          title: "Sniper Game",
+          first: {
+            rank: "1位",
+            person: "入賞者",
+            result: 0
+          },
+          second: {
+            rank: "2位",
+            person: "入賞者",
+            result: 0
+          },
+          third: {
+            rank: "3位",
+            person: "入賞者",
+            result: 0
+          },
+          fourth: {
+            rank: "4位",
+            person: "入賞者",
+            result: 0
+          },
+          fifth: {
+            rank: "5位",
+            person: "入賞者",
+            result: 0
+          },
+        }
+      }
+    }
+  },
   components: {
     StarBackGround,
   },
@@ -23,6 +209,57 @@ export default {
 .top-menu {
   position: absolute;
   z-index: 1;
-  background: rgba(97, 35, 69, 0.377);
+  width: 90%;
+  left: 0;
+  right: 0;
+}
+.title {
+  font-size: 30px;
+  padding: 140px 0 80px;
+  color: antiquewhite;
+}
+.rankings {
+  width: 92%;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-bottom: 200px;
+}
+.rankingBox {
+  background: rgba(253, 245, 230, 0.897);
+  margin: 30px 0;
+  border-radius: 20px;
+  display: block;
+  width: 47%;
+  padding-bottom: 30px;
+  border: 1px solid black;
+}
+h2 {
+  font-size: 34px;
+  padding: 30px;
+}
+table {
+  width: 100%;
+}
+tr {
+  width: 80%;
+  max-width: 700px;
+  padding: 10px 20px;
+}
+td {
+  width: 30%;
+}
+
+@media(max-width: 900px) {
+  ul {
+    flex-direction: column;
+  }
+  .rankingBox {
+    width: 100%;
+  }
 }
 </style>
--- src/views/ReviewPost.vue
+++ src/views/ReviewPost.vue
@@ -1,12 +1,99 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/reviewpage">Review Page</router-link> |
-      <router-link to="/selectgame">SelectGame</router-link>
+    <StarBackGround></StarBackGround>
+    <div class="top-menu">
+      <h1 class="title">Post your message</h1>
+      <div class="postBox">
+        <form class="form">
+          <label for="text">投稿内容</label>
+          <textarea id="text" cols="30" rows="10" v-model="reviewText" placeholder="Write your comment!"></textarea>
+        </form>
+        <button class="button" @click="sendMessage">Send message</button>
+        <router-link to="/reviewpage" class="button">Review Page</router-link>
+      </div>
     </div>
     <router-view />
     <div>Review Post</div>
   </div>
 </template>
-<script></script>
-<style scoped></style>
+<script>
+import StarBackGround from "@/components/StarBackGround.vue";
+
+export default {
+  components: {
+    StarBackGround,
+  },
+  data() {
+    return {
+      reviewText: ''
+    }
+  },
+  methods: {
+    sendMessage() {
+      // ここにreviewTextをfirebaseに送るコードを書く
+      this.reviewText = ""
+    }
+  }
+};
+</script>
+<style scoped>
+#app {
+  position: relative;
+}
+.top-menu {
+  position: absolute;
+  width: 90%;
+  left: 5%;
+  z-index: 2;
+}
+.title {
+  font-size: 30px;
+  padding: 140px 0 100px;
+  color: antiquewhite;
+}
+.postBox {
+  background: rgba(233, 232, 245, 0.178);
+  border-radius: 30px;
+  max-width: 750px;
+  margin-bottom: 200px;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+}
+.form label {
+  font-size: 20px;
+  padding: 35px 0 15px;
+  margin-left: 20%;
+  color: aliceblue;
+  letter-spacing: 3px;
+}
+.form textarea {
+  width: 80%;
+  margin-bottom: 30px;
+  background: rgba(238, 237, 240, 0.596);
+  border: 1px solid black;
+  border-radius: 10px;
+  color: aliceblue;
+  text-align: left;
+  padding: 30px;
+  font-size: 20px;
+  
+}
+.button {
+  display: inline-block;
+  text-decoration: none;
+  border: none;
+  background: none;
+  font-size: 24px;
+  width: 250px;
+  margin: 30px;
+  color: antiquewhite;
+  transition: all 1s;
+}
+.button:hover {
+  color: yellow;
+  font-size: 30px;
+}
+
+</style>
