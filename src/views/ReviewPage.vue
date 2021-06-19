cf1291d314b6eba11ef150e244fe7d596e00176e
non0829 <u474334e@ecs.osaka-u.ac.jp>
2021-06-19 18:08:24 +0900

    投稿機能の実装

--- src/views/ReviewPage.vue
+++ src/views/ReviewPage.vue
@@ -3,19 +3,29 @@
     <StarBackGround></StarBackGround>
     <div class="top-menu">
       <h1 class="title">ReviewPage</h1>
+      <ul class="buttons">
+        <li @click="displayText(collectionName[0])">Total</li>
+        <li @click="displayText(collectionName[1])">Pinballgame</li>
+        <li @click="displayText(collectionName[2])">Typinggame</li>
+        <li @click="displayText(collectionName[3])">Puzzlegame</li>
+        <li @click="displayText(collectionName[4])">Quizgame</li>
+        <li @click="displayText(collectionName[5])">Snipergame</li>
+      </ul>
       <div class="reviewBox">
         <router-link to="/reviewpost" class="reviewpost">投稿</router-link>
-        <div v-for="(review, index) in reviews" :key="index" class="review">
-          <div class="flex">
-            <img :src="review.picture" alt="" class="reviewPicture" />
-            <div class="reviewPerson">{{ review.person }}</div>
-          </div>
-          <div class="textbox">
-            <div class="textinbox">
-              {{ review.text }}
+        <transition-group name="fade">
+          <div v-for="(review, index) in reviews" :key="index" class="review">
+            <div class="flex">
+              <img :src="review.picture" alt="" class="reviewPicture" />
+              <div class="reviewPerson">{{ review.person }}</div>
+            </div>
+            <div class="textbox">
+              <div class="textinbox">
+                {{ review.text }}
+              </div>
             </div>
           </div>
-        </div>
+        </transition-group>
       </div>
     </div>
   </div>
@@ -29,12 +39,39 @@ export default {
   data() {
     return {
       reviews: [],
+      collectionName: [
+        "reviewTotal",
+        "reviewPinballGame",
+        "reviewTypingGame",
+        "reviewPuzzleGame",
+        "reviewQuizGame",
+        "reviewSniperGame",
+      ],
     };
+  },
+  methods: {
+    displayText(collectionName) {
+      this.reviews = [];
+      firebase
+        .firestore()
+        .collection(collectionName)
+        .orderBy("createdAt")
+        .limitToLast(50)
+        .get()
+        .then((snapshot) => {
+          snapshot.docs.forEach((doc) => {
+            this.reviews.unshift({
+              id: doc.id,
+              ...doc.data(),
+            });
+          });
+        });
+    },
   },
   created() {
     firebase
       .firestore()
-      .collection("reviews")
+      .collection("reviewTotal")
       .orderBy("createdAt")
       .limitToLast(50)
       .get()
@@ -50,6 +87,23 @@ export default {
   components: {
     StarBackGround,
   },
+  mounted() {
+    let buttons = document.querySelectorAll('li') 
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        if(button.classList.contains('isselected')) {
+          return
+        } else {
+          buttons.forEach((button) => {
+            if(button.classList.contains('isselected')) {
+              button.classList.remove('isselected')
+            }
+          })
+          button.classList.add('isselected')
+        }
+      })
+    })
+  }
 };
 </script>
 <style scoped>
@@ -65,6 +119,28 @@ export default {
 .title {
   padding: 140px 0 100px;
   color: aliceblue;
+}
+.buttons {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 40px;
+}
+.buttons li {
+  width: 30%;
+  margin-bottom: 20px;
+  color: white;
+  font-size: 20px;
+  padding: 10px;
+  height: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+  background: rgba(233, 232, 245, 0.178);
+  transition: all 0.7s;
+}
+.buttons li:hover {
+  color: yellow;
+  background: rgba(233, 232, 245, 0.6);
 }
 .reviewpost {
   display: block;
@@ -95,10 +171,11 @@ export default {
 .reviewBox {
   width: 100%;
   padding: 30px;
-  background: rgba(233, 232, 245, 0.178);
+  background: rgba(233, 232, 245, 0.4);
   border-radius: 30px;
   text-align: left;
   margin-bottom: 200px;
+  transition: all 1s;
 }
 .reviewPerson {
   height: 40px;
@@ -135,5 +212,30 @@ export default {
   margin-left: 0;
   text-align: left;
   padding-left: 30px;
+}
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 3s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-move {
+  transition: transform 3s;
+}
+.isselected {
+  background: rgba(241, 119, 38, 0.788) !important;
+  color: white !important;
 }
 </style>
